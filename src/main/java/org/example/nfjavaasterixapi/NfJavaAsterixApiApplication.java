1f31a0aefaba32dd77d70d7186eaae3175ff9227
nieyo <jonasbucher95@gmail.com>
2025-02-20 11:14:16 +0100

    Initial commit

---
+++ src/main/java/org/example/nfjavaasterixapi/NfJavaAsterixApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.nfjavaasterixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfJavaAsterixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfJavaAsterixApiApplication.class, args);
+	}
+
+}
